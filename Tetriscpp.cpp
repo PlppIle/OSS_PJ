2d35bc681fbc2e13f3e2240aec65d08b3b064785
PlppIle <whitney2319@naver.com>
2023-06-18 11:47:53 +0900

    누락 사항 새로 추가

--- Tetriscpp.cpp
+++ Tetriscpp.cpp
@@ -24,6 +24,7 @@ int gameBoardInfo[GBOARD_HEIGHT + 1][GBOARD_WIDTH + 2];
 int curPosX, curPosY;	// ÇöÀç Ä¿¼­ À§Ä¡ ÀúÀå º¯¼ö
 int block_id;			// ³»·Á¿Ã ºí·ÏÀÇ id
 int speed = 1;			// block ¼Óµµ Á¶Á¤
+int score = 0;			// Á¡¼ö Ãß°¡
 
 // ÇöÀç Ä¿¼­ À§Ä¡ x, y·Î º¯°æ
 void SetCurrentCursorPos(int x, int y)
@@ -47,6 +48,16 @@ COORD GetCurrentCursorPos()
 }
 
 
+// Ä¿¼­ Áö¿ì±â
+void RemoveCursor()
+{
+	CONSOLE_CURSOR_INFO curInfo;
+	GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+	curInfo.bVisible = 0;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+}
+
+
 // ºí·ÏÀ» ±×¸²
 void showBlock(char blockInfo[4][4])
 {
@@ -60,7 +71,7 @@ void showBlock(char blockInfo[4][4])
 			// Ä¿¼­¸¦ ¿òÁ÷ÀÌ¸ç 4*4 °ø°£¿¡ ºí·ÏÀ» ±×¸²
 			SetCurrentCursorPos(pos.X + x * 2, pos.Y + y);
 			if (blockInfo[y][x] == 1)
-				printf("??);
+				printf("¡á");
 		}
 		SetCurrentCursorPos(pos.X, pos.Y);
 		// Ã³À½ Ä¿¼­ À§Ä¡·Î º¹±Í
@@ -116,24 +127,24 @@ void drawBoard()
 	{
 		SetCurrentCursorPos(GBOARD_ORIGIN_X, GBOARD_ORIGIN_Y + y);
 		if (y == GBOARD_HEIGHT)
-			printf("??);
+			printf("¦¦");
 		else
-			printf("??);
+			printf("¦¢");
 	}
 
 	for (x = 1; x <= GBOARD_WIDTH + 1; x++)
 	{
 		SetCurrentCursorPos(GBOARD_ORIGIN_X + (x * 2) - 1, GBOARD_ORIGIN_Y + GBOARD_HEIGHT);
-		printf("?€");
+		printf("¦¡");
 	}
 
 	for (y = 0; y <= GBOARD_HEIGHT; y++)
 	{
 		SetCurrentCursorPos(GBOARD_ORIGIN_X + (GBOARD_WIDTH * 2) + 2, GBOARD_ORIGIN_Y + y);
 		if (y == GBOARD_HEIGHT)
-			printf("??);
+			printf("¦¥");
 		else
-			printf("??);
+			printf("¦¢");
 	}
 }
 
@@ -221,6 +232,74 @@ void SpaceDown()
 	while (BlockDown());
 }
 
+// °ÔÀÓº¸µå¿¡ ºí·Ï Á¤º¸¸¦ ÀúÀå
+void AddBlockToBoard()
+{
+	// 4*4 ¿µ¿ªÀ» µ¹¸é¼­ °ÔÀÓº¸µåÀÇ 0ÀÎ ºÎºÐÀ» 1·Î º¯°æÇÏ¿© ºí·Ï Á¤º¸ ÀúÀå
+	int x, y, arrCurX, arrCurY;
+	for (y = 0; y < 4; y++)
+	{
+		for (x = 0; x < 4; x++)
+		{
+			arrCurX = (curPosX - GBOARD_ORIGIN_X) / 2;
+			arrCurY = curPosY - GBOARD_ORIGIN_Y;
+
+			if (blockModel[block_id][y][x] == 1)
+				gameBoardInfo[arrCurY + y][arrCurX + x] = 1;
+		}
+	}
+}
+
+// Ãß°¡µÈ ºí·Ï Á¤º¸¸¦ Æ÷ÇÔÇÏ¿© °ÔÀÓº¸µå¸¦ ´Ù½Ã ±×¸²
+void RedrawBlocks()
+{
+	int x, y;
+	int cursX, cursY;
+
+	for (y = 0; y < GBOARD_HEIGHT; y++)
+	{
+		for (x = 1; x < GBOARD_WIDTH + 1; x++)
+		{
+			cursX = x * 2 + GBOARD_ORIGIN_X;
+			cursY = y + GBOARD_ORIGIN_Y;
+			SetCurrentCursorPos(cursX, cursY);
+			if (gameBoardInfo[y][x] == 1)
+				printf("¡á");
+			else
+				printf("  ");
+		}
+	}
+}
+
+// ÇÑ ¶óÀÎÀÌ ¸ðµÎ ºí·ÏÀ¸·Î °¡µæ Ã¡À» °æ¿ì ÇØ´ç ¶óÀÎ Á¦°Å
+void RemoveFillUpLine()
+{
+	int y, x, line;
+
+	// °ÔÀÓº¸µå ÀüÃ¼¸¦ µ¼
+	for (y = GBOARD_HEIGHT - 1; y > 0; y--)
+	{
+		for (x = 1; x < GBOARD_WIDTH + 1; x++)
+		{
+			if (gameBoardInfo[y][x] != 1)
+				break;
+		}
+
+		// ÀüÃ¼¸¦ µ¹´Ù°¡ ¾î´À ÇÑ ÁÙÀÌ Ã³À½ºÎÅÍ ¸¶Áö¸·±îÁö 1ÀÏ °æ¿ì
+		// ÇÑ ÁÙÀÌ ¿Ï¼ºµÆ´Ù°í ÆÇ´Ü
+		if (x == GBOARD_WIDTH + 1)
+		{
+			// ÇØ´ç ÁÙÀ» ÃÊ±âÈ­
+			for (line = 0; y - line > 0; line++)
+				memcpy(&gameBoardInfo[y - line][1], &gameBoardInfo[(y - line) - 1][1], GBOARD_WIDTH * sizeof(int));
+			// ÁÙÀ» ÇÏ³ª¾¿ ¾Æ·¡·Î ³»¸²
+			y += 1;
+			score += 10;	// ÁÙ Á¦°Å ½Ã Á¡¼ö È¹µæ
+		}
+	}
+	RedrawBlocks();
+}
+
 // Å° ÀÔ·ÂÀ» ¹ÞÀ½
 void ProcessKeyInput()
 {
@@ -253,13 +332,31 @@ void ProcessKeyInput()
 	}
 }
 
+// ºí·ÏÀÌ ÀÏÁ¤ ¼öÁØ ÀÌ»óÀ¸·Î ¿Ã¶ó¿À¸é °ÔÀÓ Á¾·á
+int IsGameOver()
+{
+	if (!DetectCollision(curPosX, curPosY, blockModel[block_id]))
+		return 1;
+
+	return 0;
+}
+
+// ÇöÀç Á¡¼ö¸¦ º¸¿©ÁÖ´Â ÀÎÅÍÆäÀÌ½º
+void showScore()
+{
+	SetCurrentCursorPos(30, 10);
+	printf("Score : %d", score);
+	SetCurrentCursorPos(curPosX, curPosY);
+}
+
 
 // main
 int main()
 {
-
 	srand(time(NULL));
-  
+
+	RemoveCursor();
+
 	while (1)
 	{
 		// ºí·Ï ·£´ý »ý¼º
@@ -269,11 +366,22 @@ int main()
 
 		drawBoard();
 
+		if (IsGameOver())
+		{
+			SetCurrentCursorPos(8, 1);
+			printf("Game Over!!");
+			break;
+		}
+
 		while (1)
 		{
+			showScore();
 
 			if (BlockDown() == 0)	// Å×Æ®¸®½º¿¡¼­ ºí·ÏÀº °è¼Ó ³»·Á¿È
 			{
+				// ÀÌÈÄ Ãæµ¹ ½Ã 0À» ¹ÝÈ¯
+				// ºí·ÏÀ» ÇöÀç À§Ä¡¿¡ ÀúÀå
+				// °ÔÀÓº¸µå¸¦ ´Ù½Ã ±×¸²
 				AddBlockToBoard();
 				RemoveFillUpLine();
 				break;
@@ -282,6 +390,7 @@ int main()
 		}
 	}
 
+	getchar();
 
 	return 0;
 }
