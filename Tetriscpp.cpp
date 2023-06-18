0316a32606a46b34ef503162c44082d205f82519
PlppIle <whitney2319@naver.com>
2023-06-18 11:00:20 +0900

    block show,delete

--- Tetriscpp.cpp
+++ Tetriscpp.cpp
@@ -15,6 +15,9 @@
 // 게임보드
 int gameBoardInfo[GBOARD_HEIGHT + 1][GBOARD_WIDTH + 2];
 
+int curPosX, curPosY;	// 현재 커서 위치 저장 변수
+int block_id;			// 내려올 블록의 id
+
 // 현재 커서 위치 x, y로 변경
 void SetCurrentCursorPos(int x, int y)
 {
@@ -34,4 +37,59 @@ COORD GetCurrentCursorPos()
 	curPoint.Y = curInfo.dwCursorPosition.Y;
 
 	return curPoint;
+}
+
+// 블록을 그림
+void showBlock(char blockInfo[4][4])
+{
+	// 현재 커서의 위치 반환
+	COORD pos = GetCurrentCursorPos();
+
+	for (int x = 0; x < 4; x++)
+	{
+		for (int y = 0; y < 4; y++)
+		{
+			// 커서를 움직이며 4*4 공간에 블록을 그림
+			SetCurrentCursorPos(pos.X + x * 2, pos.Y + y);
+			if (blockInfo[y][x] == 1)
+				printf("■");
+		}
+		SetCurrentCursorPos(pos.X, pos.Y);
+		// 처음 커서 위치로 복귀
+	}
+}
+
+// 블록을 지움
+void deleteBlock(char blockInfo[4][4])
+{
+	COORD pos = GetCurrentCursorPos();
+
+	for (int x = 0; x < 4; x++)
+	{
+		for (int y = 0; y < 4; y++)
+		{
+			// 커서를 움직이며 4*4 공간에 그렸던 블록을 지움
+			SetCurrentCursorPos(pos.X + x * 2, pos.Y + y);
+			if (blockInfo[y][x] == 1)
+				printf("  ");
+		}
+		SetCurrentCursorPos(pos.X, pos.Y);
+		// 처음 커서 위치로 복귀
+	}
+}
+
+// main
+int main()
+{
+	srand(time(NULL));
+
+	// 블록 랜덤 생성
+	block_id = rand() % 28;
+	curPosX = GBOARD_ORIGIN_X + 6;
+	curPosY = GBOARD_ORIGIN_Y;
+
+	showBlock(blockModel[block_id]);
+	deleteBlock(blockModel[block_id]);
+
+	return 0;
 }
